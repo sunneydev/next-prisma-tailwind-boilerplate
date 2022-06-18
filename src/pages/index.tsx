fbb77bdb525ee781d43ee2efa15139ee8cc64bd2
sunney-x <sanishubitidze@live.com>
2022-06-18 20:43:09 +0400

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return (
+    <div className="bg-black h-screen flex items-center justify-center text-white text-2xl">
+      <h1>Next.js + Prisma + TailwindCSS boilerplate</h1>
+    </div>
+  );
+};
+
+export default Home;
